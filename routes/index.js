ddc49b0bcff5f9668643650dfb2a2307181abfb5
Alex Ellis <alexellis2@gmail.com>
2019-10-31 14:29:50 +0000

    Make ajax call to API via jQuery
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,36 @@ module.exports = {
     main: (req, res) => {
         res.type('html')
         res.send(`<html>
-                <h3>Hi, thanks for using my Express.js lab 👏</h3>
-                <p>If you found it useful, <a href="https://github.com/users/alexellis/sponsorship">view my other GitHub projects</a> today 👑</p>
-                <p>Alex Ellis</p>
-                </html>
-    `);
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<title>Welcome to Alex's express.js example</title>
+</head>
+<body>
+    <h3>Hi, thanks for using my Express.js lab 👏</h3>
+
+    <p>If you found it useful, <a href="https://github.com/alexellis/">view my other GitHub projects</a> today 👑</p>
+
+    <p>Results from <quote>GET <a href="/links">/links</a></quote>:</p>
+
+    <div id="links">
+    </div>
+
+    <p>Copyright Alex Ellis 2019 &reg;</p>
+    <script>
+    $(document).ready(function(){
+        $.get("/links", function(data, status){
+            var res = "<ul>";
+            var links = data;
+            for(var i=0; i < links.length; i++) {
+                res += "<li><a href=" + links[i].url + ">" + links[i].name + "</a></li>"
+            }
+            $("#links").html(res)
+        });
+    });
+    </script>
+    </body>
+</html>
+`);
     },
     links: (req, res) => {
         res.type("json")
