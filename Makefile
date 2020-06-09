c7d32aba09c0ba16ca8181e1af87b20a2d0e0ea5
Angel Nuñez <snahider@gmail.com>
2020-06-09 09:21:11 -0500

    Setup workshop

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: charts
-all: charts
-
-charts:
-	cd chart && helm package expressjs-k8s/
-	mv chart/*.tgz docs/
-	helm repo index docs --url https://alexellis.github.io/expressjs-k8s/ --merge ./docs/index.yaml
--- app.js
+++ app.js
@@ -9,29 +9,6 @@ app.get('/',function(req,res){
 app.get('/health',function(req,res){
     res.send("OK");
 });
-  
-app.get('/links',function(req,res){
-    res.type("json")
-    res.send(
-        [
-            {
-                "name": "github",
-                "url": "https://github.com/alexellis"
-            },
-            {
-                "name": "twitter",
-                "url": "https://twitter.com/alexellisuk"
-            },
-            {
-                "name": "blog",
-                "url": "https://blog.alexellis.io"
-            },
-            {
-                "name": "sponsors",
-                "url": "https://github.com/users/alexellis/sponsorship"
-            },
-        ])
-});
 
 app.listen(4000, () => {
     console.log(`Express.js listening on port 4000`)
